8c00b9bb79f24da7fafb63c5078b96ee96d72ac3
alkanoic <naotoinaguma@hotmail.com>
2017-09-14 22:41:11 +0900

    プロジェクト ファイルを追加します。

---
+++ CsvReaderWriter/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvReaderWriter
+{
+    public class Class1
+    {
+    }
+}
